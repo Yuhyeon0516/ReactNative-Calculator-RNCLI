f8ff0a2f05f14502b9ad0f94d558e295f17c2bb7
Yuhyeon0516 <rladbgus0516@naver.com>
2023-08-02 19:27:11 +0900

    write CalculatorPakage

---
+++ android/app/src/main/java/com/calculator_rncli/CalculatorPakage.kt
@@ -0,0 +1,15 @@
+package com.calculator_rncli
+
+import android.view.View
+import com.facebook.react.ReactPackage
+import com.facebook.react.bridge.NativeModule
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.uimanager.ReactShadowNode
+import com.facebook.react.uimanager.ViewManager
+
+class CalculatorPakage:ReactPackage {
+    override fun createNativeModules(context: ReactApplicationContext): MutableList<NativeModule> = listOf(CalculatorModule(context)).toMutableList()
+
+    override fun createViewManagers(context: ReactApplicationContext): MutableList<ViewManager<View, ReactShadowNode<*>>> = mutableListOf()
+
+}
